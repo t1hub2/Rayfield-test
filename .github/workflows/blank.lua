5d9168b98a329acdc6c50e71e2d9d3e92a0dfa3d
t1hub2 <solavolodimir@gmail.com>
2024-12-01 18:15:23 +0100

    Update blank.lua

--- .github/workflows/blank.lua
+++ .github/workflows/blank.lua
@@ -44,3 +44,12 @@ local Button = Tab:CreateButton({
    -- The function that takes place when the button is pressed
    end,
 })
+local Toggle = Tab:CreateToggle({
+   Name = "Toggle Example",
+   CurrentValue = false,
+   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+   -- The function that takes place when the toggle is pressed
+   -- The variable (Value) is a boolean on whether the toggle is true or false
+   end,
+})
